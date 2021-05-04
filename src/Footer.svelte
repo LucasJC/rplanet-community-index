2f63a956710e1d49abc0492d70dd4801664e79d8
LucasJC <lucas.couchot@gmail.com>
2021-05-04 18:23:52 -0300

    update repo url

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -6,7 +6,7 @@
   <footer class="footer">
     <div class="content has-text-centered">
       <p>Wax address for donations: <strong>glrrk.wam</strong></p>
-      <p>Sources available at <a href="https://github.com/LucasJC/rplanet-index">github</a>. <strong>LucasJC</strong></p>
+      <p>Sources available at <a href="https://github.com/LucasJC/rplanet-community-index">github</a>. <strong>LucasJC</strong></p>
       <p><a href="mailto:lucas.couchot+rplidx@gmail.com">Contact me</a></p>
     </div>
   </footer>
